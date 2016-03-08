e152efa2b606a2aecc64752ecd8dde91477bfbe0
Dave Moten <davidmoten@gmail.com>
2016-03-08 17:03:04 +1100

    add hashtable

---
+++ src/main/java/algorithms/HashTable.java
@@ -0,0 +1,23 @@
+package algorithms;
+
+public class HashTable {
+
+    private final int[] table;
+
+    public HashTable() {
+        table = new int[16];
+    }
+
+    public void put(int key, int value) {
+
+    }
+
+    public void get(int key) {
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Integer.highestOneBit(1000) << 1);
+    }
+
+}
