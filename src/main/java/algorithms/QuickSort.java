2c26914dc025193617dcdf4e03c094a58cb3c52a
Dave Moten <davidmoten@gmail.com>
2016-03-27 22:26:40 +1100

    dev

--- src/main/java/algorithms/QuickSort.java
+++ src/main/java/algorithms/QuickSort.java
@@ -23,7 +23,7 @@ public final class QuickSort {
 	}
 
 	private static int partition(int[] a, int from, int to) {
-		int v = a[(from+ to)/2];// pivot value
+		int v = a[(from + to) / 2];// pivot value (arbitrary)
 		int i = from;
 		int j = to;
 		while (i <= j) {
@@ -34,19 +34,22 @@ public final class QuickSort {
 				j--;
 			}
 			if (i <= j) {
-				// swap the two values
-				int x = a[i];
-				a[i] = a[j];
-				a[j] = x;
+				swap(a, i, j);
 				i++;
 				j--;
 			}
 		}
-		return i-1;
+		return i - 1;
+	}
+
+	private static void swap(int[] a, int i, int j) {
+		int temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
 	}
 
 	public static void main(String[] args) {
-		int[] a = { 5,4,3,2,1};
+		int[] a = { 5, 4, 3, 2, 1, 6,7,8,3,2,1 };
 		sort(a);
 		System.out.println(Arrays.toString(a));
 	}
